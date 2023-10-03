644bf440ef517251c0c78458fb4edbb24cbe9f48
Dan Webb <dan.webb@damacus.io>
2023-10-03 16:03:09 +0100

    Remove some nvim plugins

--- nvim/config/lua/plugins/lazy.lua
+++ nvim/config/lua/plugins/lazy.lua
@@ -1,15 +1,5 @@
 return {
   {
-    "lukas-reineke/indent-blankline.nvim",
-    event = "BufReadPost",
-    opts = {
-      char = "│",
-      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-      show_trailing_blankline_indent = false,
-      show_current_context = false,
-    },
-  },
-  {
     "echasnovski/mini.indentscope",
     version = false, -- wait till new 0.7.0 release to put it back on semver
     event = "BufReadPre",
