ef3b5b7de76f1d1667a25156b2b003edc0927986
Dan Webb <dan.webb@damacus.io>
2023-10-03 15:48:28 +0100

    feat: Add abbreviations for commands in install scripts
    This commit adds abbreviations for certain commands in the install scripts. The changes include:
    - Replacing aliases with `abbr` command in `macos/install.fish`, `nvim/install.fish`, `system/install.fish`, `terraform/install.fish`, and `yubikey/conf.d/yubikey.fish`
    - Adding abbreviations for commands like `airport`, `afk`, `e`, `v`, `vim`, etc.
    
    These changes improve code readability and make it easier to use the commands.

--- system/install.fish
+++ system/install.fish
@@ -1,13 +1,13 @@
 #!/usr/bin/env fish
 abbr -a less 'less -r'
 
-if command -qs exa
-    alias --save l='exa -lh --icons'
-    alias --save ll='exa -l --icons'
-    alias --save lt='exa -l --icons --tree --level=2'
+if command -qs eza
+    abbr -a l='eza -lh --icons'
+    abbr -a ll='eza -l --icons'
+    abbr -a lt='eza -l --icons --tree --level=2'
 else
-    alias --save l='ls -lAh'
-    alias --save ll='ls -l'
+    abbr -a l='ls -lAh'
+    abbr -a ll='ls -l'
 end
 
 if command -qs fdfind
@@ -18,7 +18,7 @@ if command -qs batcat
     ln -sf (which batcat) ~/.bin/bat
 end
 if command -qa bat
-    alias --save cat=bat
+    abbr -a cat=bat
     set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p'"
 end
 
